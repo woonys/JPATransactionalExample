cb63a572efc8f32ca82c56f3b8c321cc54e23d62
woonys <wodns1324@naver.com>
2022-06-02 16:13:49 +0900

    springBoot setting

---
+++ src/main/java/springboot/Application.java
@@ -0,0 +1,10 @@
+package springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class Application { // 프로젝트의 메인 클래스
+        public static void main(String[] args) {
+            SpringBootApplication.run(Application.class, args);
+        }
+}
