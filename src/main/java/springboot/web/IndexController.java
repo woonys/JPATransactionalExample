3dfdbf2f24a8d8e376bafb7fb94e7e3594e4b76f
woonys <wodns1324@naver.com>
2022-06-12 20:18:48 +0900

    Ch4.2-BasicPage

---
+++ src/main/java/springboot/web/IndexController.java
@@ -0,0 +1,21 @@
+package springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+}
